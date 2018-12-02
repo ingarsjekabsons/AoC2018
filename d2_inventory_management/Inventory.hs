399814fb3e11da892818600b220a703d051a39db
Ingars Jekabsons <dzingars@gmail.com>
2018-12-02 17:21:56 +0200

    day2 part2

--- d2_inventory_management/Inventory.hs
+++ d2_inventory_management/Inventory.hs
@@ -27,9 +27,33 @@ countSignificants a b = ((fst b) + x, (snd b) + y)
 count :: (Int, Int) -> String -> (Int, Int)
 count counts s = countSignificants (countElems s) counts
 
+differsBy :: String -> String -> Int
+differsBy (s:ss) (s':ss') = case (s == s') of
+                                True  -> differsBy ss ss'
+                                False -> 1 + differsBy ss ss'
+differsBy [] [] = 0
+differsBy _  _  = 0
+
+produceList :: [String] -> Int -> [String]
+produceList (s:ss) i = take i (repeat s) ++ produceList ss i
+produceList [] _     = []
+
+findDiffersByOne :: [String] -> [String] -> (String, String)
+findDiffersByOne (s:ss) (s':ss') = case (differsBy s s') of
+                                       1 -> (s, s')
+                                       _ -> findDiffersByOne ss ss'
+findDiffersByOne [] _            = ("Not found","Not found")
+
+getCommonChars :: String -> String -> String
+getCommonChars (s:ss) (s':ss') = case (s == s') of
+                                    True  -> s : getCommonChars ss ss'
+                                    False -> getCommonChars ss ss'
+getCommonChars [] _            = []
 
 main :: IO ()
 main = do
     input <- readFile "data.txt"
-    let r = foldl count (0,0) (lines input) 
+    let r = foldl count (0,0) (lines input)
+    let rr = findDiffersByOne (produceList (lines input) (length $ lines input)) (cycle $ lines input)
     putStrLn ("Result is: " ++ show ((fst r) * (snd r)))
+    putStrLn ("Result part 2 is: \n " ++ show (getCommonChars (fst rr) (snd rr)))

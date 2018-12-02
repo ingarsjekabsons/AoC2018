bbb0f05a9c5c8c6249f530b122eee9c15dc02a14
Ingars Jekabsons <dzingars@gmail.com>
2018-12-02 13:37:06 +0200

    day1 part2

--- d1_chronal_calibration/Calibration.hs
+++ d1_chronal_calibration/Calibration.hs
@@ -32,15 +32,22 @@ partiallyApplyFuns :: [((Int -> Int -> Int), Int)] -> [(Int -> Int)]
 partiallyApplyFuns ((f,i):fs) = (f i):(partiallyApplyFuns fs)
 partiallyApplyFuns []         = []
 
-
 chainFuns :: [(Int -> Int)] -> Int -> Int
 chainFuns (f:fs) i = chainFuns fs (f i)
 chainFuns []     i = i
 
+chainFuns2 :: [(Int -> Int)] -> Int -> [Int] -> Int
+chainFuns2 (f:fs) curVal allVals = case (curVal `elem` allVals) of
+                                     True -> curVal
+                                     False -> chainFuns2 fs (f curVal) (curVal:allVals)
+
 getResult :: String -> Int
 getResult s = chainFuns (partiallyApplyFuns $ getListOfInstrs $ parseInput s) 0
 
+getResult2 :: String -> Int
+getResult2 s = chainFuns2 (cycle $ partiallyApplyFuns $ getListOfInstrs $ parseInput s) 0 []
 main :: IO ()
 main = do
     input <- readFile "data.txt"
     putStrLn ("Result is: " ++ show (getResult input))
+    putStrLn ("Result p2 is: " ++ show (getResult2 input))
